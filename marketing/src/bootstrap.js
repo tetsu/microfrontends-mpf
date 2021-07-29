1915875d8de5b2d7b0a4d9ffd714aaa6f80d475d
Tetsuro Mori <tetsuromori@gmail.com>
2021-07-30 00:15:50 +0900

    add bootstrap

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Mount function to start up the app
+const mount = (el) => {
+    ReactDOM.render(
+        <h1>Hi There</h1>,
+        el
+    );
+};
+
+// If we are indevelopment and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector('#_marketing-dev-root');
+
+    if(devRoot){
+        mount(devRoot);
+    }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount };
